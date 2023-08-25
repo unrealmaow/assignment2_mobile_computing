c05df43c47e6ea008bc988396c886d4c5722f49b
Sameel Butt <officialsameel@gmail.com>
2023-08-25 19:04:34 +0500

    initial release with react native

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>RollNo: 201370025</Text>
+      <Text>Name: Muhammad Sameel Butt</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
